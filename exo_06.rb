d4509ecf9ebf29c405fea716eeeec5dceda42c72
blueeden-63 <chloe.delagneau6315@gmail.com>
2026-01-13 19:52:16 +0100

    add 1 exercise ruby

--- exo_06.rb
+++ exo_06.rb
@@ -3,6 +3,6 @@ puts "Choisissez un nombre ?"
 print "> "
 number_1 = gets.chomp.to_i
 number_2 = number_1 - 1
-number_2.times do '|Bonjour toi !|'
+number_2.times do |i|
     puts 'Bonjour toi !'
 end
